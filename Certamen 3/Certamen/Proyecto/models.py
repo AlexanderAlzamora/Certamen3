c62fd13a02f3696ca4b652da51faac1221c620a1
CamiloThenoux <167055174+CamiloThenoux@users.noreply.github.com>
2024-07-05 19:29:49 -0400

    Actualizacion Proyecto
    Actualizacion Proyecto Camilo Thenoux

--- Certamen 3/Certamen/Proyecto/models.py
+++ Certamen 3/Certamen/Proyecto/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+Turnito=[
+    ("AM","AM"),
+    ("PM","PM"),
+    ("MM","MM"),
+]
+
+
+
+#Class Carrera
+class Registro(models.Model):
+    codigoCombustible = models.CharField(max_length=10)
+    litrosCombustibles = models.CharField(max_length=80)
+    Fecha = models.IntegerField()
+    Turno = models.CharField(max_length=80,choices=Turnito)
+    hora = models.CharField(max_length=80)
+
+
+
+    def __str__(self) -> str:
+       return self.Turno
--- Certamen 3/Certamen/Proyecto/views.py
+++ Certamen 3/Certamen/Proyecto/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    title = "Inicio"
+
+    data = {
+        "title" : title,
+    }
+
+    return render(request, 'core/home.html',data)
